f546fe299e23595a3e046dbdd71094a8f6ac7e99
Gaurav Rastogi <gaurav.rstogi@gmail.com>
2018-09-27 03:26:37 +0530

    Small Changes

--- Delivery/Delivery/Services/Network/APIClient.swift
+++ Delivery/Delivery/Services/Network/APIClient.swift
@@ -10,6 +10,13 @@ import Alamofire
 import Foundation
 
 struct APIClient {
+    
+    /// A static/class method used to get deliveries from server
+    ///
+    /// - Parameters:
+    ///   - offset: Starting offset/index for delivery data being fetched
+    ///   - limit: Number of items per page being fetched
+    ///   - completion: A Completion handler passing Result<[Delivery],Error>
     static func get(deliveriesWith offset:Int, limit:Int, completion:@escaping (Result<[Delivery],DataResponseError>)->Void) {
         Alamofire.request(APIRouter.deliveries(offset: offset, limit: limit))
             .responseJSON { (response) in
